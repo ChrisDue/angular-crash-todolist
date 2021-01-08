e899961ff5314dd6914758c860e77591e2e92b72
Christopher Dührkop <christopher.duehrkop@jobleads.com>
2021-01-08 21:20:14 +0100

    add about page and route to it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
 import { TodosComponent } from './components/todos/todos.component';
 
 const routes: Routes = [
-  { path: '', component: TodosComponent }
+  { path: '', component: TodosComponent }, // Blank for root
+  { path: 'about', component: AboutComponent } // About
 ];
 
 @NgModule({
